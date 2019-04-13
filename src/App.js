37b083ae218b337a35a5fb54e101fcd8b61f769c
Pyriaxis <winter.scaze@gmail.com>
2019-04-13 11:58:41 +0800

    UI updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 /* https://react.semantic-ui.com/ */
-import { Button, Grid, Container } from 'semantic-ui-react'
+import { Grid, Container, Divider, Segment, Image, Header } from 'semantic-ui-react'
 import AWS from 'aws-sdk';
 
 import Build from './Build';
@@ -14,16 +14,23 @@ class App extends Component {
     return (
       <div className="App">
         <Container>
-          <Grid columns='three' divided>
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Build></Build>
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <Deploy></Deploy>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+          <Header as='h1'>
+            <Image src = 'nektar.jpg' size='small' rounded></Image> Nektar
+          </Header>
+          <Segment>
+            <Grid columns='two' relaxed>
+              <Grid.Row>
+                <Grid.Column width={8}>
+                  <Build></Build>
+                </Grid.Column>
+                <Grid.Column width={8}>
+                  <Deploy></Deploy>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+
+            <Divider vertical>THEN</Divider>
+          </Segment>
         </Container>
       </div>
     );
