ff7ee83db6188e3e1d3542f5cd3ac53bc28749ad
Ronald Kan <ronald_kan@tech.gov.sg>
2019-04-13 11:57:51 +0800

    added aws creds

--- src/App.js
+++ src/App.js
@@ -5,8 +5,39 @@ import './App.css';
 /* https://react.semantic-ui.com/ */
 import { Button, Grid, Container } from 'semantic-ui-react'
 import AWS from 'aws-sdk';
+import uuid from 'uuid';
+
 
 class App extends Component {
+
+  componentDidMount() {
+    var creds = new AWS.Credentials('AKIAQ722XR6B2NNPH3WZ', 'ardJbvvUuIp/kd1gZtekI/4FOGzPldxykpMdMUNa');
+    AWS.config.credentials = creds;
+    // Create unique bucket name
+    var bucketName = 'sdk-test1';
+    // Create name for uploaded object key
+    var keyName = 'hello_world.txt';
+
+    // Create a promise on S3 service object
+    var bucketPromise = new AWS.S3({ apiVersion: '2006-03-01' }).createBucket({ Bucket: bucketName }).promise();
+
+    // Handle promise fulfilled/rejected states
+    bucketPromise.then(
+      function (data) {
+        // Create params for putObject call
+        var objectParams = { Bucket: bucketName, Key: keyName, Body: 'Hello World!' };
+        // Create object upload promise
+        var uploadPromise = new AWS.S3({ apiVersion: '2006-03-01' }).putObject(objectParams).promise();
+        uploadPromise.then(
+          function (data) {
+            console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+          });
+      }).catch(
+        function (err) {
+          console.error(err, err.stack);
+        });
+  }
+
   render() {
     return (
       <div className="App">
